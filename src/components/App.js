0e679b5e2463586e212d08b78044e6da7c5d047a
kambleps <125243027+kambleps@users.noreply.github.com>
2023-07-15 03:14:15 +0000

    one

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let[status,setStatus]=useState(false);
+  function handle(){
+    setStatus(true)
+  }
   return (
     <div id="main">
       // Do not alter the main div
+      <button id="click" onClick={handle}>Click Me</button>
+      {
+        status&&<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      }
     </div>
   );
 }
